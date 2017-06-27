a0120984860cfa1fa24caaaef2e1a70484896e6e
chickenCabbage <alon.shiboleth@gmail.com>
2017-06-27 21:39:59 +0300

    put everything in 1 function and added random image placment

--- resources/index.js
+++ resources/index.js
@@ -7,16 +7,22 @@ window.onLoad = jQuery.get("/update.txt", function(data, status) { //init
 	document.getElementById("lu-time").innerHTML = updateText.split("	")[0] + " : Last update";
 	document.getElementById("lu-title").innerHTML = "Update title : " + updateText.split("	")[1];
 	document.getElementById("lu-panel").src = updateText.split("	")[2];
-});
 
-window.onLoad = setInterval(function() {
-	document.getElementById("status").innerHTML = "Checked " + lastCheckTime + " seconds ago."
-	if (lastCheckTime == 60){ //reset the check loop
-		check(); //refresh
-		lastCheckTime = 0;
-	}
-	lastCheckTime ++;
-}, 1000);
+	var img = document.getElementById("lu-panel");
+	var width = img.clientWidth;
+	var height = img.clientHeight + (img.clientHeight / 4);
+	var offset = (Math.random() * (height - (height / 2))) - (height / 2)
+	img.style.bottom = offset + "px";
+
+	setInterval(function() {
+		document.getElementById("status").innerHTML = "Checked " + lastCheckTime + " seconds ago."
+		if (lastCheckTime == 10){ //reset the check loop
+			updateInfo(); //refresh
+			lastCheckTime = 0;
+		}
+		lastCheckTime ++;
+	}, 1000);
+});
 
 function updateInfo() {
 	document.getElementById("status").innerHTML = "Checking for update...";
