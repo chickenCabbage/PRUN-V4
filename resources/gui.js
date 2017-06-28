f4be7f03e46e48097f75fafbaae38e4f941f9cca
chickenCabbage <alon.shiboleth@gmail.com>
2017-06-28 21:40:24 +0300

    added navbar
    to-do: add the users system and images in the topbar

--- resources/gui.js
+++ resources/gui.js
@@ -1,10 +1,9 @@
 function toggleNav() {
 	var nav = document.getElementById("nav");
-	if(nav.style.top == "0%") {
+	if(nav.style.top == "-10%") {
 		nav.style.top = "95%";
 	}
 	else {
-		nav.style.top = "0%";
-		document.getElementById("arrow");
+		nav.style.top = "-10%";
 	}
 }
